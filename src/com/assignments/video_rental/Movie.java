359af3342c83aeff705a5860f570368574091e61
bhawna sharma <44021335+sharmabhawna@users.noreply.github.com>
2019-03-29 21:08:26 +0530

    Intorduce PriceCode entity and delegated the task of calculating rental points and rental price to PriceCode

--- src/com/assignments/video_rental/Movie.java
+++ src/com/assignments/video_rental/Movie.java
@@ -22,38 +22,13 @@ class Movie {
 	}
 
 	int rentalPoints(int daysRented) {
-		if(this.priceCode == Movie.NEW_RELEASE && daysRented > 1){
-			return 2;
-		}
-		return 1;
+		PriceCode priceCode = new PriceCode(this.priceCode);
+		return priceCode.calculateRenterPoint(daysRented);
 	}
 
 	double rentToPay(int daysRented) {
-		double rentalPrice = 0;
-		int leaseDays = 0;
-		double rentPerDay = 1.5;
-
-		if (this.priceCode == Movie.REGULAR) {
-			rentalPrice = 2;
-			leaseDays = 2;
-		}
-
-		if (this.priceCode == Movie.CHILDREN) {
-			rentalPrice = 1.5;
-			leaseDays = 3;
-		}
-
-		if (this.priceCode == Movie.NEW_RELEASE) {
-			rentPerDay = 3;
-		}
-
-		int excessiveDays = daysRented - leaseDays;
-
-		if (daysRented > leaseDays) {
-			rentalPrice +=  excessiveDays * rentPerDay;
-		}
-
-		return rentalPrice;
+		PriceCode priceCode = new PriceCode(this.priceCode);
+		return priceCode.calculateRent(daysRented);
 	}
 }
 
--- src/com/assignments/video_rental/PriceCode.java
+++ src/com/assignments/video_rental/PriceCode.java
@@ -1,14 +1,53 @@
 package com.assignments.video_rental;
 
-public class PriceCode {
+import java.util.HashMap;
+import java.util.Map;
 
-    private final int basePrice;
-    private final int baseDays;
-    private final double rentalPrice;
+class PriceCode {
 
-    public PriceCode(int basePrice, int baseDays, double rentalPrice) {
-        this.basePrice = basePrice;
-        this.baseDays = baseDays;
-        this.rentalPrice = rentalPrice;
+    private int price;
+    private Map<Integer, PriceCodeTypes> priceCodes;
+
+    PriceCode(int price) {
+        this.price = price;
+        priceCodes = new HashMap<>(3);
+        priceCodes.put(0, PriceCodeTypes.REGULAR);
+        priceCodes.put(1, PriceCodeTypes.NEW_RELEASE);
+        priceCodes.put(2, PriceCodeTypes.CHILDREN);
     }
+
+    double calculateRent(int daysRented) {
+        PriceCodeTypes priceCodeType = priceCodes.get(this.price);
+        double rent = 0;
+
+        if(daysRented > priceCodeType.leaseDays){
+            rent = (daysRented - priceCodeType.leaseDays) * priceCodeType.rentPerDay;
+        }
+
+        return priceCodeType.rentalPrice + rent;
+    }
+
+    int calculateRenterPoint(int daysRented) {
+        if(this.price == 1 && daysRented > 1){
+            return 2;
+        }
+        return 1;
+    }
+
+    private enum PriceCodeTypes {
+        CHILDREN(1.5,3,1.5),
+        REGULAR(2,2,1.5),
+        NEW_RELEASE(0,0,3);
+
+        private final double rentalPrice;
+        private final int leaseDays;
+        private final double rentPerDay;
+
+         PriceCodeTypes(double rentalPrice, int leaseDays, double rentPerDay) {
+            this.rentalPrice = rentalPrice;
+            this.leaseDays = leaseDays;
+            this.rentPerDay = rentPerDay;
+        }
+    }
+
 }
